5a42ab884a9bb5193da03b352e7c6196197ea12d
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-02-27 01:04:26 +0200

    fix code with flake8

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import json
 
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
-    return json.dumps(serializer.data).encoded('utf-8')
+    return json.dumps(serializer.data).encoded("utf-8")
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    json_str = json.decode('utf-8')
+    json_str = json.decode("utf-8")
     data = json.loads(json_str)
-    
+
     return Car(**data)
